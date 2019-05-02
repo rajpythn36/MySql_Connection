9536bed306e03d2d216587b37d2908eaeed692c9
rajveer28 <rajveersolanki28@gmail.com>
2019-05-02 15:09:48 -0700

    Joining cust1 and cust2 and extracting salary,fullname

--- MySqlConnector.py
+++ MySqlConnector.py
@@ -12,3 +12,11 @@ for row in records:
     InsertQuery = "INSERT INTO cust2 (fullname, eid) VALUES (%s,%s)"
     cursor.execute(InsertQuery, insertvalues)
     connection.commit()
+
+JoinQuery =  "select cust2.fullname, cust1.salary from cust1 INNERJOIN cust2 ON cust1.eid = cust2.eid where salary between 5000 and 8000"
+cursor.execute(JoinQuery)
+records1 = cursor.fetchall()
+for fullname, salary in records1:
+    print(f"{fullname}, {salary}")
+cursor.close()
+mySQLconnection.close()
