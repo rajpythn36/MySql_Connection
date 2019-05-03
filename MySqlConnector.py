2931055666e61ae83e952df6d62e7696cab9be6b
rajveer28 <rajveersolanki28@gmail.com>
2019-05-02 18:48:59 -0700

    Count total salary per city for particular customer

--- MySqlConnector.py
+++ MySqlConnector.py
@@ -3,7 +3,7 @@ mySQLconnection = mysql.connector.connect(host='localhost',
                              database='Cust_db',
                              user='User_dev',
                              password='123456')
-Query = "select fname, lname, eid from cust1"
+Query = "SELECT fname, lname, eid FROM cust1"
 cursor = mySQLconnection .cursor()
 cursor.execute(Query)
 records = cursor.fetchall()
@@ -13,10 +13,17 @@ for row in records:
     cursor.execute(InsertQuery, insertvalues)
     connection.commit()
 
-JoinQuery =  "select cust2.fullname, cust1.salary from cust1 INNERJOIN cust2 ON cust1.eid = cust2.eid where salary between 5000 and 8000"
+JoinQuery =  "SELECT cust2.fullname, cust1.salary FROM cust1 INNERJOIN cust2 ON cust1.eid = cust2.eid WHERE salary BETWEEN 5000 and 8000"
 cursor.execute(JoinQuery)
 records1 = cursor.fetchall()
 for fullname, salary in records1:
     print(f"{fullname}, {salary}")
+
+NewQuery = 'SELECT COUNT(salary), CITY FROM cust1 WHERE lname LIKE 'A%S' GROUP BY city'
+cursor.execute(NewQuery)
+records2 = cursor.fetchall()
+for salary, city in records2:
+    print(f"{salary}, {city}")
+
 cursor.close()
 mySQLconnection.close()
